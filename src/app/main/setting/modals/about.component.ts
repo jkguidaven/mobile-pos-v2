0269b1c6560377555d716b4653f56d3c19c8d069
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-05-02 22:22:41 +0800

    Add about page

--- src/app/main/setting/modals/about.component.ts
+++ src/app/main/setting/modals/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-setting-about',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingAboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
 
+  dismiss() {
+    this.modalController.dismiss();
+  }
 }
